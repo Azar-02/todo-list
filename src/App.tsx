d21ed21bbab6737db6159a7960031a863333bfc3
Mohamed Azarudeen <mailforazarudeen@gmail.com>
2025-03-08 22:15:45 +0530

    added css in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Todo from "./Todo"
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <Todo/>
     </div>
   )
